027b3b72c93f1feb25572b324b032de28b5b8470
Nusratillo <nxursanjonov888@gmail.com>
2024-06-20 21:37:39 +0500

    adds

---
+++ src/components/changeFileImg.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChangeFileImg = () => {
+    return (
+        <div>ChangeFileImg</div>
+    )
+}
+
+export default ChangeFileImg
--- src/pages/admin/create/Create.jsx
+++ src/pages/admin/create/Create.jsx
@@ -1,18 +1,44 @@
-import React, { memo } from 'react'
+import React, { Fragment, memo, useState } from 'react'
+import { useGetValue } from '../../../hooks/useGetValue'
+
+const initialState = {
+    title: '',
+    price: '',
+    oldPrice: '',
+    catgory: '',
+    units: '',
+    description: '',
+    info: '',
+}
 
 const Create = () => {
+    const { formData, handleChange } = useGetValue(initialState)
+    const [files, setFiles] = useState('')
+
     const handleCreateProduct = (e) => {
         e.preventDefault()
         console.log(object);
     }
 
     return (
-        <form onSubmit={handleCreateProduct}>
-            <h2>Create Products</h2>
-            <input autoFocus type="text" name="title" placeholder="title" className='input input-info font-semibold bg-white' />
-            <input type="number" name="price" placeholder="price" className='input input-info font-semibold bg-white' />
-            <input type="number" name="oldPrice" placeholder="oldPrice" className='input input-info font-semibold bg-white' />
-        </form>
+        <Fragment>
+            <form onSubmit={handleCreateProduct} className='w-[600px] grid grid-cols-1 gap-3 font-semibold'>
+                <h2>Create Products</h2>
+                <input value={formData?.title} onChange={handleChange}
+                    autoFocus type="text" name="title" placeholder="title" className='input input-info font-semibold bg-white' />
+                <input value={formData?.price} onChange={handleChange}
+                    type="number" name="price" placeholder="price" className='input input-info font-semibold bg-white' />
+                <input value={formData?.oldPrice} onChange={handleChange}
+                    type="number" name="oldPrice" placeholder="oldPrice" className='input input-info font-semibold bg-white' />
+                <input value={formData?.catgory} onChange={handleChange}
+                    type="text" name="category" placeholder="category" className='input input-info font-semibold bg-white' />
+                <input value={formData?.units} onChange={handleChange}
+                    type="text" name="units" placeholder="units" className='input input-info font-semibold bg-white' />
+                <div>
+                    <input onChange={e => { setFiles(e.target.files) }} accept='image/*' type="file" name="file" />
+                </div>
+            </form>
+        </Fragment>
     )
 }
 
